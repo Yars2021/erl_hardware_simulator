9c29f1e524e6e9611d934beb952f29ef29ed0235
Yars2021 <yaroslav.sukhovey@yandex.ru>
2024-11-27 01:31:54 +0300

    Shape classifier neural net model

--- components/control_unit.erl
+++ components/control_unit.erl
@@ -7,30 +7,34 @@ execute(LocalMem, Bus, RAM, Memory) ->
     receive
         {start_calc} ->
             receive {clk} -> Memory ! {get_num_of_layers, self()} end, % Ask for layer count
-            receive {clk} -> receive {num_of_layers, Layers} -> RAM ! {get_layer_len, self()} end end, % Ask for layer size
-            receive {clk} -> receive {layer_len, LayerLen} ->
-                input_cycle(LocalMem, Bus, RAM, LayerLen),
-                layer_cycle(LocalMem, Bus, RAM, Memory, Layers)
+            receive {clk} -> receive {num_of_layers, Layers} ->
+                receive {clk} ->
+                    layer_cycle(LocalMem, Bus, RAM, Memory, Layers),
+                    receive {clk} -> RAM ! {send_to_output, Bus} end % Send last layer to IO
+                end
             end end
     end.
 
 
-% Input layer calc cycle
-input_cycle(LocalMem, Bus, RAM, LayerSize) ->
-    Iterations = round(math:ceil(LayerSize / length(LocalMem))),
-
-    broadcast(LocalMem, {erase}), % Clear local memory
-    receive {clk} -> RAM ! {send_to_calc_distribute, Bus} end, % Send layer inputs to cores
-    receive {clk} -> RAM ! {erase} end, % Clear RAM
-    receive {clk} -> receive {clk} -> looped_broadcast(Iterations, LocalMem, {calc, vector_mul}) end end, % Calculate activations
-    receive {clk} -> sequential_looped_broadcast(Iterations, LocalMem, {get_result, Bus}) end, % Collect calculated input layer in RAM
-    receive {clk} -> RAM ! {send_to_output, Bus} end. % Debug RAM output
-
-
 % Full layer calc cycle
-layer_cycle(LocalMem, Bus, RAM, Memory, Layers) ->
-    % Get current layer size and do calc cycles until Layers is 0
-    0.
+layer_cycle(_, _, _, _, 0) -> 0;
+
+layer_cycle(LocalMem, Bus, RAM, Memory, CalcNum) ->
+    % Get current layer size and do calc cycles until CalcNum is 0
+    receive {clk} -> RAM ! {get_layer_len, self()} end,
+    receive {clk} -> receive {layer_len, LayerLen} ->
+        Iterations = round(math:ceil(LayerLen / length(LocalMem))),
+
+        broadcast(LocalMem, {erase}), % Clear local memory.
+        receive {clk} -> RAM ! {send_to_calc, Bus} end, % Send layer inputs to cores
+        receive {clk} -> Memory ! {send_weights_to_calc, CalcNum, Bus} end, % Send weights to cores
+        receive {clk} -> RAM ! {erase} end, % Clear RAM
+        receive {clk} -> receive {clk} -> looped_broadcast(Iterations, LocalMem, {calc, inputs_and_weights}) end end, % Calculate weighted input sum
+        receive {clk} -> receive {clk} -> looped_broadcast(Iterations, LocalMem, {calc, vector_mul}) end end, % Calculate activations
+        receive {clk} -> sequential_looped_broadcast(Iterations, LocalMem, {get_result, Bus}) end % Collect calculated layer in RAM
+    end end,
+
+    receive {clk} -> layer_cycle(LocalMem, Bus, RAM, Memory, CalcNum - 1) end.
 
 
 looped_broadcast(0, _, _) -> 0;
